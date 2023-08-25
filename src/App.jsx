9b5a825bb7a64306c5815d4141c37fa1d5cb4087
efrvame <efrvame@gmail.com>
2023-08-25 12:11:51 -0500

    Add a Navbar.jsx component for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
     return (
         <>
-            <h1>Meme generator app</h1>
+            <Navbar />
         </>
     );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <header>
+            <div className="container">
+                <nav>
+                    <h1 className="logo">Meme Generator</h1>
+                    <h3 className="extra-title">React Course - Project 3</h3>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;
