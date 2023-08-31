a12f18e1c2b3fb47709aaee7d4fd1dc03f2d1655
efrvame <efrvame@gmail.com>
2023-08-31 18:07:27 -0500

    Add getMemes async function inside our useEffect hook.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,11 +9,14 @@ function Meme() {
 
     const [allMemes, setAllMemes] = useState([]);
 
-    useEffect(function () {
-        fetch("https://api.imgflip.com/get_memes")
-            .then((res) => res.json())
-            .then((data) => setAllMemes(data.data.memes));
-    });
+    useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes");
+            const data = await res.json();
+            setAllMemes(data.data.memes);
+        }
+        getMemes();
+    }, []);
 
     function getRandomMeme() {
         const randomIndex = Math.floor(Math.random() * allMemes.length);
